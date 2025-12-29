b88df22aa59acad490fb27cfcc1a7e53616a9ff6
Shaan Khan K <pip000470@gmail.com>
2025-12-29 20:05:23 +0500

    Update 0out.js

--- Script/commands/0out.js
+++ Script/commands/0out.js
@@ -5,7 +5,7 @@ module.exports.config = {
     credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "",
     commandCategory: "Admin",
-    usages: "out [id]",
+    usages: "left [id]",
     cooldowns: 10,
 };
 
